816f6bbb62441b7cdeb319dae5fd95348dec0550
Flo-Schilli <github@flo.schilli.dev>
2025-01-10 20:15:27 +0100

    #6 Added Grohe Blue Home to List of device names

--- custom_components/grohe_smarthome/dto/grohe_device.py
+++ custom_components/grohe_smarthome/dto/grohe_device.py
@@ -66,6 +66,8 @@ class GroheDevice:
             return 'Sense Guard'
         elif dev_name == GroheTypes.GROHE_SENSE_PLUS:
             return 'Sense Plus'
+        elif dev_name == GroheTypes.GROHE_BLUE_HOME:
+            return 'Blue Home'
         elif dev_name == GroheTypes.GROHE_BLUE_PROFESSIONAL:
             return 'Blue Professional'
         else:
