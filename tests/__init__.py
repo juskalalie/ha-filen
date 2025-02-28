93224aef2c71621d15ba41a355f7701d60d618c8
juskalalie <42800189+juskalalie@users.noreply.github.com>
2025-02-28 15:00:05 +0100

    Delete tests directory

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from datetime import datetime, timedelta
-
-async def bootstrap():
-    print(datetime.now().astimezone() - timedelta(minutes=25))
-    print(datetime.now())
-
-
-asyncio.run(bootstrap())
