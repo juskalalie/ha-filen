2830ba41bf3827eec00c3b2b0d2b1056352b6322
juskalalie <42800189+juskalalie@users.noreply.github.com>
2025-02-28 14:45:15 +0100

    Delete custom_components/grohe_smarthome/entities directory

--- custom_components/grohe_smarthome/entities/interface/coordinator_button_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import abstractmethod
-from typing import Dict
-
-
-class CoordinatorButtonInterface:
-    @abstractmethod
-    async def send_command(self, data_to_send: Dict[str, any]) -> Dict[str, any]:
-        raise NotImplementedError
